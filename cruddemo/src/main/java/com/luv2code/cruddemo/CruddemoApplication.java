2287be91858314fd9d99eef7433fba4f7ce1bf91
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-01 21:57:30 -0400

    Creating queries

--- cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -20,9 +22,20 @@ public class CruddemoApplication {
 		return runner -> {
 //			createStudent(studentDAO);
 //			createMultipleStudents(studentDAO);
+//			readStudent(studentDAO);
 
-			readStudent(studentDAO);
+			queryForStudents(studentDAO);
 		};
+	}
+
+	private void queryForStudents(StudentDAO studentDAO) {
+		// get a list of students
+		List<Student> theStudents = studentDAO.findAll();
+
+		// display list of students
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
 	}
 
 	private void readStudent(StudentDAO studentDAO) {
--- cruddemo/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
+++ cruddemo/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -28,5 +31,14 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("from Student order by lastName asc", Student.class);
+
+        // return query results
+        return theQuery.getResultList();
     }
 }
